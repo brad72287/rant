215b06ed7541c15ec2af5b2c8bfcda6527e680fa
Bradley Reiss <bradleyreiss@Bradleys-MacBook-Air.local>
2016-09-27 13:51:42 -0400

    began adding some routes

---
+++ app/assets/javascripts/routes.js
@@ -0,0 +1,23 @@
+(function(){
+
+	'use strict';
+
+	angular
+		.module('app')
+		.config(function($stateProvider, $urlRouterProvider){
+			$stateProvider
+				.state('home',{
+					url: '/',
+					templateUrl: 'home/home.html',
+					controller: 'HomeController as vm'
+				})
+				.state('home.tweets',{
+					url: '/',
+					templateUrl: 'tweets/tweets.html',
+					controller: 'TweetsController as vm'
+				})
+			$urlRouterProvider.otherwise('/');
+		})
+
+
+}());
